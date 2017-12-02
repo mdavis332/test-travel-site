ba0747fc112b5305a68bc234f48db8a67c4f3504
Michael Davis <mdavis332@gmail.com>
2017-12-02 14:08:51 -0600

    thru lect 47: finished reveal-on-scroll css and js

--- app/assets/scripts/App.js
+++ app/assets/scripts/App.js
@@ -1,3 +1,7 @@
 import MobileMenu from './modules/MobileMenu';
+import RevealOnScroll from './modules/RevealOnScroll';
+import $ from 'jquery';
 
 var mobileMenu = new MobileMenu();
+var featureRevealOnScroll = new RevealOnScroll($(".feature-item"), "85%");
+var testimonialRevealOnScroll = new RevealOnScroll($(".testimonial"), "60%");
