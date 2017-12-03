ea5cdccb1e6ba484bc5561ab6b7216bb0e102a8f
Michael Davis <mdavis332@gmail.com>
2017-12-02 18:21:32 -0600

    thru lect 60: completed gulp build task

--- gulpfile.js
+++ gulpfile.js
@@ -1,4 +1,5 @@
 require('./app/gulp/tasks/styles');
 require('./app/gulp/tasks/watch');
 require('./app/gulp/tasks/sprites');
-require('./app/gulp/tasks/scripts');;
+require('./app/gulp/tasks/scripts');
+require('./app/gulp/tasks/build');
